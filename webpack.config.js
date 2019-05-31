958c441d7c2392a2e8c27be7a56781932b4c805e
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-05-31 19:24:03 -0300

    Include ico extension

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
         }
       }]
     }, {
-      test: /\.(png|jpe?g)/i,
+      test: /\.(png|jpe?g|ico)/i,
       use: [{
           loader: 'url-loader',
           options: {
